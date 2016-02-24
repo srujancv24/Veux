480840bf098fefcd324f7a17b82a85d1420bf4d4
Venkata Srujan Chalasani <vchalasa@syr.edu>
2016-02-24 07:04:14 -0500

    Initial Commit

---
+++ Party_hardy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Party_hardy
+//
+//  Created by Venkata Srujan Chalasani on 2/24/16.
+//  Copyright © 2016 Venkata Srujan Chalasani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
